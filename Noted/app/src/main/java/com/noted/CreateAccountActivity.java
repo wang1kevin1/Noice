d0af76efc6f3b4be5ea6c35a2d99c5e2cfe3534a
sean.bell <sean.bell@servicenow.com>
2019-05-13 13:04:25 -0700

    Account: CreateAccountActivity and LoginActivity working properly

--- Noted/app/src/main/java/com/noted/CreateAccountActivity.java
+++ Noted/app/src/main/java/com/noted/CreateAccountActivity.java
@@ -1,13 +1,120 @@
 package com.noted;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.noted.utils.AccountUtil;
+
+/**
+ * A login screen that offers login via email/password.
+ */
 public class CreateAccountActivity extends AppCompatActivity {
+
+    private static final String TAG = "Sign In";
+
+    /**
+     * Firebase Connection
+     */
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+
+    // UI references.
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private EditText mConfirmPasswordEditText;
+    private EditText mUsernameEditText;
+    private Button mCreateAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        // Text Inputs
+        mEmailEditText = findViewById(R.id.editTextEmail);
+        mPasswordEditText = findViewById(R.id.editTextPassword);
+        mConfirmPasswordEditText = findViewById(R.id.editTextConfirmPassword);
+        mUsernameEditText = findViewById(R.id.editTextUsername);
+
+        // Buttons
+        mCreateAccountButton = findViewById(R.id.buttonCreateAccount);
+
+        mCreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Set errors to null
+                mPasswordEditText.setError(null);
+                mEmailEditText.setError(null);
+
+                if(mPasswordEditText.getText().toString().equals(mConfirmPasswordEditText.getText().toString())) {
+                    mCreateAccountButton.setEnabled(false);
+                    createAccount(mEmailEditText.getText().toString().trim(), mPasswordEditText.getText().toString(), mUsernameEditText.getText().toString().trim());
+                } else {
+                    mPasswordEditText.setError("Passwords do not match");
+                    mConfirmPasswordEditText.setError("Passwords do not match");
+                }
+
+
+
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    private void createAccount(String email, String password, final String username){
+
+        if(email.isEmpty())
+            mEmailEditText.setError("Email field is required.");
+
+        else if(!AccountUtil.isEmailValid(email))
+            mEmailEditText.setError("Please enter a valid email");
+
+
+        else if(AccountUtil.isPasswordValid(password, mPasswordEditText)){
+
+            //Attempt to create an account
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                AccountUtil.writeUser(user.getUid(), user.getEmail(), username);
+
+                                Intent myIntent = new Intent(CreateAccountActivity.this,
+                                        DashboardActivity.class);
+                                mCreateAccountButton.setEnabled(true);
+                                startActivity(myIntent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
--- Noted/app/src/main/java/com/noted/LoginActivity.java
+++ Noted/app/src/main/java/com/noted/LoginActivity.java
@@ -1,13 +1,149 @@
 package com.noted;
-
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.noted.utils.AccountUtil;
+
+/**
+ * A login screen that offers login via email/password.
+ */
 public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    private static final String TAG = "Sign In";
+
+    /**
+     * Firebase Connection
+     */
+    private FirebaseAuth mAuth;
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+
+    // UI references.
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private TextView mCreateAccountButton;
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Buttons
+        mEmailEditText = findViewById(R.id.editTextEmail);
+        mPasswordEditText = findViewById(R.id.editTextPassword);
+        mCreateAccountButton = findViewById(R.id.buttonCreateAccountActivity);
+        mLoginButton = findViewById(R.id.buttonLogin);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptLogin(mEmailEditText.getText().toString().trim(), mPasswordEditText.getText().toString().trim());
+            }
+        });
+
+        mCreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LoginActivity.this,
+                        CreateAccountActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+
+    private void attemptLogin(String email, String password) {
+        //Set errors to null
+        mPasswordEditText.setError(null);
+        mEmailEditText.setError(null);
+
+        if(email.isEmpty())
+            mEmailEditText.setError("Email field is required.");
+
+        else if(!AccountUtil.isEmailValid(email))
+            mEmailEditText.setError("Please enter a valid email");
+
+        else{
+
+            //Attempt to login to an account
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "loginUserWithEmail:success");
+                                Intent myIntent = new Intent(LoginActivity.this,
+                                        DashboardActivity.class);
+                                startActivity(myIntent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "loginUserWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+        }
+    }
+
+ /*   @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideInput(v, ev)) {
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+            return super.dispatchTouchEvent(ev);
+        }
+        if (getWindow().superDispatchTouchEvent(ev)) {
+            return true;
+        }
+        return onTouchEvent(ev);
+    }
+    public  boolean isShouldHideInput(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] leftTop = { 0, 0 };
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+            int bottom = top + v.getHeight();
+            int right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+*/
 }
